b85208811dab10bca07ba1b16d7ebfa64a6884c7
Ahmad El-Banna <ahmed_elbanna159@yahoo.com>
2023-01-23 18:34:34 +0200

    adding unubtrusive lib to make client-side validation works properly

--- DTOModels/UniversityDTO.cs
+++ DTOModels/UniversityDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 using trial_project_for_MVC_Core.Interfaces;
 
@@ -7,10 +8,14 @@ namespace trial_project_for_MVC_Core.Models
     public class UniversityDTO:IBasicInfo
     {
         public int Id { get; set; }
+
+        [Remote(action: "CheckName", controller: "University", ErrorMessage = "Name Already Exist",AdditionalFields ="Id")]
         [StringLength(30,ErrorMessage = "Max Length of the name is 30)")]
         [Display(Name ="University Name")]
         [Required(ErrorMessage = "Please enter university name")]
-        public string Name { get; set; }
+        public string Name{ get; set; }
+        
+        
         [StringLength(40)]
         [Display(Name ="university Location")]
         public string Location { get; set; }
